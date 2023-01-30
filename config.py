3e1adb0289dd6ed1f37dcf00d33f7570ac2a87e9
SHOKDHAR <121552501+918088529@users.noreply.github.com>
2023-01-30 16:12:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,8 +12,8 @@ API_HASH = os.environ.get("API_HASH", "2576e0b5c6675d1728f71e8241899a9e")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "6197417600:AAHQI0jw5rZPK5u8Rfx2RwIP0x2sgJraY18")
 ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("854143836")] if os.environ.get("ADMINS") else []
 ADMIN = ADMINS
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "cluster0")
-DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://shokdhar:42444000@cluster0.pagmwax.mongodb.net/?retryWrites=true&w=majority") 
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "Linkconvertner")
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://shok123:shok123@cluster0.pagmwax.mongodb.net/?retryWrites=true&w=majority") 
 OWNER_ID =  int(os.environ.get("OWNER_ID", "854143836")) 
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 ADMINS.append(Id Owned Id)
